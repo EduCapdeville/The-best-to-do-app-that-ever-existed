20228afd5d76f333f2cb82f3de8b6046ba2493c9
Eduardo Capdeville <capdevilleeduardo@gmail.com>
2024-04-03 15:29:37 -0300

    :hammer: Create functions that make the tasks saved localy, and make the buttons delete and check functional

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ function addTask() {
         li.appendChild(span);
     }
     taskInput.value = "";
+    saveData();
 }
+
+taskList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", taskList.innerHTML);
+}
+
+function showTask(){
+    taskList.innerHTML = localStorage.getItem("data");
+}
+showTask();
